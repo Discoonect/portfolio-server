349ee60ce319cf84e18e0b760278c1aec7eda5a2
jeonganWkd <67768119+jeonganWkd@users.noreply.github.com>
2020-08-17 17:51:59 +0900

    회원탈퇴코드
    회원탈퇴 코드 추가

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { createUser, login, logout } = require("../controllers/user");
+const { createUser, login, logout, adios } = require("../controllers/user");
 
 const auth = require("../middleware/auth");
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.route("/").post(createUser);
 router.route("/login").post(login);
 router.route("/logout").delete(auth, logout);
+router.route("/adios").delete(auth, adios);
 
 module.exports = router;
