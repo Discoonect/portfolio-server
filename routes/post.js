d738651306983564538a5777a486006e9feec14e
jeonganWkd <67768119+jeonganWkd@users.noreply.github.com>
2020-08-18 11:40:20 +0900

    포스팅 업로드

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
+const { upload } = require("../controllers/post");
 
 const auth = require("../middleware/auth");
 
 const router = express.Router();
+
+router.route("/").post(auth, upload);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
-
+const fileupload = require("express-fileupload");
+const path = require("path");
 const morgan = require("morgan");
 
 const user = require("./routes/user");
@@ -9,6 +10,8 @@ const post = require("./routes/post");
 
 const app = express();
 app.use(express.json());
+app.use(fileupload());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(morgan("dev"));
 
