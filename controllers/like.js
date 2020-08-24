5d1601fdc618d6a3cb8ac1e424f3dbfbf6ef7bad
jeonganWkd <67768119+jeonganWkd@users.noreply.github.com>
2020-08-24 17:09:08 +0900

    댓글 좋아요 처리

--- controllers/like.js
+++ controllers/like.js
@@ -31,20 +31,73 @@ exports.likepost = async (req, res, next) => {
 
 //@desc             게시글 좋아요 취소
 //@route            DELETE/api/v1/like/deletelikepost
-//@request          postlike_id
+//@request          postlike_id, user_id(auth)
 //@response         success
 exports.deletelikepost = async (req, res, next) => {
-  let post_id = req.body.post_id;
+  let postlike_id = req.body.postlike_id;
   let user_id = req.user.id;
 
-  if (!post_id || !user_id) {
+  if (!postlike_id || !user_id) {
     res
       .status(400)
       .json({ success: false, message: "요청을 찾을 수 없습니다" });
     return;
   }
-  let query = "delete from postlike where post_id = ? and user_id = ?";
-  let data = [post_id, user_id];
+  let query = "delete from postlike where id = ? and user_id = ?";
+  let data = [postlike_id, user_id];
+
+  try {
+    [result] = await connection.query(query, data);
+    res.status(200).json({ success: true, message: "좋아요가 취소되었습니다" });
+  } catch (e) {
+    res.status(500).json({ success: false, error: e });
+  }
+};
+
+//@desc             댓글 좋아요 하기
+//@route            POST/api/v1/like/likecomment
+//@request          comment_id, user_id(auth)
+//@response         success
+exports.likecomment = async (req, res, next) => {
+  let comment_id = req.body.comment_id;
+  let user_id = req.user.id;
+
+  let query = "insert into commentlike (comment_id, user_id) values (?,?)";
+  let data = [comment_id, user_id];
+
+  try {
+    [result] = await connection.query(query, data);
+    res.status(200).json({ success: true, message: "이 댓글을 좋아합니다" });
+    return;
+  } catch (e) {
+    //중복 좋아요 방지
+    if (e.errno == 1062) {
+      res
+        .status(500)
+        .json({ success: false, message: "이미 이 댓글을 좋아합니다" });
+      return;
+    } else {
+      res.status(500).json({ success: false, error: e });
+    }
+  }
+};
+
+//@desc             댓글 좋아요 취소
+//@route            DELETE/api/v1/like/deletelikecomment
+//@request          commentlike_id, user_id(auth)
+//@response         success
+exports.deletelikecomment = async (req, res, next) => {
+  let commentlike_id = req.body.commentlike_id;
+  let user_id = req.user.id;
+
+  if (!commentlike_id || !user_id) {
+    res
+      .status(400)
+      .json({ success: false, message: "요청을 찾을 수 없습니다" });
+    return;
+  }
+  let query = "delete from commentlike where id = ? and user_id = ?";
+  let data = [commentlike_id, user_id];
 
   try {
     [result] = await connection.query(query, data);
--- routes/like.js
+++ routes/like.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
-const { likepost, deletelikepost } = require("../controllers/like");
+const {
+  likepost,
+  deletelikepost,
+  likecomment,
+  deletelikecomment,
+} = require("../controllers/like");
 
 const auth = require("../middleware/auth");
 
@@ -8,4 +13,6 @@ const router = express.Router();
 
 router.route("/likepost").post(auth, likepost);
 router.route("/deletelikepost").delete(auth, deletelikepost);
+router.route("/likecomment").post(auth, likecomment);
+router.route("/deletelikecomment").delete(auth, deletelikecomment);
 module.exports = router;
