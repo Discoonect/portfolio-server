6f2f4a70bd17342b5040c3abf46564b720605210
jeonganWkd <67768119+jeonganWkd@users.noreply.github.com>
2020-09-03 22:15:13 +0900

    다양한시도중

--- controllers/search.js
+++ controllers/search.js
@@ -9,15 +9,16 @@ exports.searchuser = async (req, res, next) => {
   let user = req.query.user;
   let offset = req.query.offset;
   let limit = req.query.limit;
+  let string = "@"
   let query = `select u.user_name, u.user_profilephoto \
                 from user as u \ 
                 where u.user_name like "%${user}%" \
                 limit ${offset}, ${limit}`;
   try {
     [rows] = await connection.query(query);
-    //if (validator.contains("@"+user)) {
+    if (user.includes(string)==true) {
       res.status(200).json({ success: true, items: rows });
-   // }
+   }
   } catch (e) {
     res.status(500).json({ success: false, error: e });
   }
