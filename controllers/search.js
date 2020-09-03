fddc2687ac594d40531b842ab6b4a136050372ff
jeonganWkd <67768119+jeonganWkd@users.noreply.github.com>
2020-09-03 21:33:08 +0900

    수정중

--- controllers/search.js
+++ controllers/search.js
@@ -1,7 +1,8 @@
 const connection = require("../db/mysql_connection");
+const validator = require("validator");
 
 //@desc             유저 검색
-//@route            GET/api/v1/searchuser?keyword=@a
+//@route            GET/api/v1/searchuser?user=@a
 //@request          user_id
 //@response         success, items
 exports.searchuser = async (req, res, next) => {
@@ -10,14 +11,13 @@ exports.searchuser = async (req, res, next) => {
   let limit = req.query.limit;
   let query = `select u.user_name, u.user_profilephoto \
                 from user as u \ 
-                where u.user_name like  %${user}%" \
+                where u.user_name like "%${user}%" \
                 limit ${offset}, ${limit}`;
   try {
     [rows] = await connection.query(query);
-    if(user.contains("@") == true){
-      res.status(200).json({ success: true, items: rows });  
+    if (validator.contains("@" + user)) {
+      res.status(200).json({ success: true, items: rows });
     }
-    
   } catch (e) {
     res.status(500).json({ success: false, error: e });
   }
--- routes/post.js
+++ routes/post.js
@@ -15,8 +15,8 @@ const auth = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/uploadpost").post(auth, uploadpost);
-router.route("/getpostphotourl/:user_id").get(auth, getpostphotourl);
-router.route("/getonepost/:post_id").get(auth, getonepost);
+router.route("/getpostphotourl/:user_id").get(getpostphotourl);
+router.route("/getonepost/:post_id").get(getonepost);
 router.route("/getallpost").get(auth, getallpost);
 router.route("/updatepost/:post_id").put(auth, updatepost);
 router.route("/deletepost/:post_id").post(auth, deletepost);
