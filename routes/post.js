0add4c64e8de9a5c184dccb5afd6fc2176129c57
jeonganWkd <67768119+jeonganWkd@users.noreply.github.com>
2020-10-07 11:30:56 +0900

    인기게시물표시 api변경3

--- routes/post.js
+++ routes/post.js
@@ -18,5 +18,5 @@ router.route("/upload").post(auth, upload);
 router.route("/photourl/:user_id").get(photourl);
 router.route("/allpost").get(auth, allpost);
 router.route("/:post_id").put(auth, update).delete(auth, deletepost).get(auth, one);
-router.route("/best").get(auth, best);
+router.route("/best").get(best);
 module.exports = router;
