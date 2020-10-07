bca812dab311a3a410b2abd54f765089fe10abef
jeonganWkd <67768119+jeonganWkd@users.noreply.github.com>
2020-10-07 11:43:14 +0900

    인기게시물표시 api변경중

--- routes/post.js
+++ routes/post.js
@@ -14,9 +14,10 @@ const auth = require("../middleware/auth");
 
 const router = express.Router();
 
+router.route("/best").get(best);
 router.route("/upload").post(auth, upload);
 router.route("/photourl/:user_id").get(photourl);
 router.route("/allpost").get(auth, allpost);
 router.route("/:post_id").put(auth, update).delete(auth, deletepost).get(auth, one);
-router.route("/best").get(best);
+
 module.exports = router;
