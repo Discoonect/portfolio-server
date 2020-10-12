357a5f895535fe5723b0e44df7917370119a5bfe
jeonganWkd <67768119+jeonganWkd@users.noreply.github.com>
2020-10-12 11:20:34 +0900

    회원가입 주석 req수정

--- controllers/user.js
+++ controllers/user.js
@@ -8,7 +8,7 @@ const connection = require("../db/mysql_connection");
 
 //@desc             회원가입
 //@route            POST/api/v1/user/signup
-//@request          user_name, user_passwd, user_phone, user_nickname
+//@request          user_name, user_passwd, user_phone
 //@response         success, token
 exports.signup = async (req, res, next) => {
   let name = req.body.user_name;
